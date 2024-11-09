f11ff8a23f4858cdbad2c958990bdf56c3c3ae84
FreddyA12 <2balvarezfreddy@gmail.com>
2024-11-08 22:56:05 -0500

    Inicializando el proyecto

---
+++ src/main/java/com/practica/practicaRest/PracticaRestApplication.java
@@ -0,0 +1,13 @@
+package com.practica.practicaRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaRestApplication.class, args);
+	}
+
+}
