877a3034bd1d4802b5a881e4de42e9f43bda7600
FreddyA12 <2balvarezfreddy@gmail.com>
2024-11-09 00:25:25 -0500

    Creacion de las entidades customer y address y sus tablas respectivas

---
+++ src/main/java/com/practica/practicaRest/entities/Address.java
@@ -0,0 +1,26 @@
+package com.practica.practicaRest.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Table (name = "addresses")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private boolean principal;
+    private String province;
+    private String city;
+    private String address;
+    @ManyToOne()
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
